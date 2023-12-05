fc1d2d1429d561ac2ccf11a1afdb5a7d11f4ef34
Danilo Schleu <daniloschleu@gmail.com>
2023-12-04 23:10:04 -0300

    fix: text resume

--- src/pages/Home/components/Testimony.tsx
+++ src/pages/Home/components/Testimony.tsx
@@ -22,6 +22,9 @@ export function Testimony() {
     },
   ];
 
+  const resumedText = (text: string, size = 300) =>
+    text.length < size ? text : `${text.slice(0, size)}...`;
+
   return (
     <div className="w-full flex justify-center">
       <div className="w-full max-w-screen-cut flex flex-col items-center gap-10 px-4 md:px-6">
@@ -34,7 +37,7 @@ export function Testimony() {
               key={item.text}
               className="md:max-w-[33%] w-full border border-light-300 rounded-xl px-6 py-4 "
             >
-              <p className="text-light-900">{item.text}</p>
+              <p className="text-light-900">{resumedText(item.text)}</p>
               <h3 className="text-light-200 mt-2">
                 {item.author} - Discente {name}
               </h3>
